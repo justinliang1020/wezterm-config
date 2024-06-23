5cf296df2c001dcd4b7e7ae6228c55889b49ce12
Justin Liang <justinliang1020@gmail.com>
2024-06-23 16:11:31 -0400

    add styles

--- wezterm.lua
+++ wezterm.lua
@@ -7,10 +7,10 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.keys = {
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
 
 config.font_size = 18.0
@@ -18,34 +18,34 @@ config.font_size = 18.0
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
 local function get_appearance()
-	if wezterm.gui then
-		return wezterm.gui.get_appearance()
-	end
-	return "Dark"
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
 end
 
 config.background = {
-	{
-		source = {
-			File = (function()
-				local appearance = get_appearance()
-				if appearance:find("Dark") then
-					return wezterm.home_dir .. "/.config/wezterm/lain-dark.png"
-				else
-					return wezterm.home_dir .. "/.config/wezterm/lain-light.png"
-				end
-			end)(),
-		},
-	},
+  {
+    source = {
+      File = (function()
+        local appearance = get_appearance()
+        if appearance:find("Dark") then
+          return wezterm.home_dir .. "/.config/wezterm/lain-dark.png"
+        else
+          return wezterm.home_dir .. "/.config/wezterm/lain-light.png"
+        end
+      end)(),
+    },
+  },
 }
 
 config.color_scheme = (function()
-	local appearance = get_appearance()
-	if appearance:find("Dark") then
-		return "flexoki-dark"
-	else
-		return "flexoki-light"
-	end
+  local appearance = get_appearance()
+  if appearance:find("Dark") then
+    return "flexoki-dark"
+  else
+    return "flexoki-light"
+  end
 end)()
 config.hide_tab_bar_if_only_one_tab = true
 
